1f016ffcff6a9e18081a5e33083e6b0458ff6609
Douglas Hanley <thesecretaryofwar@gmail.com>
2021-01-29 15:06:47 -0500

    set to html highlighting

--- src/core.go
+++ src/core.go
@@ -190,7 +190,7 @@ func Run(opts *Options, version string, revision string) {
 			eventBox.Unwatch(EvtReadNew)
 			eventBox.WaitFor(EvtReadFin)
 
-			high := NewHighlighter(HighlightANSI, slab)
+			high := NewHighlighter(HighlightHTML, slab)
 
 			snapshot, _ := chunkList.Snapshot()
 			merger, _ := matcher.scan(MatchRequest{
